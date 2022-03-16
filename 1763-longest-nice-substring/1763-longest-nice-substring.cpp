a93c3382438c00a1ecd48144c24c749dd78e9bad
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-03-16 19:24:24 +0530

    Time: 8 ms (77.47%), Space: 12.2 MB (49.79%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.cpp
+++ 1763-longest-nice-substring/1763-longest-nice-substring.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     string longestNiceSubstring(string s) {
+        // if(s.size()<2) return "";
+        // unordered_set<char>set;
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     set.insert(s[i]);
+        // }
+        // for(int i=0;i<s.size();i++)
+        // {
+        //     if((set.find(tolower(s[i]))!=set.end()) && (set.find(toupper(s[i])))!=set.end())
+        //     {
+        //         continue;
+        //     }
+        //     string prev=longestNiceSubstring(s.substr(0,i));
+        //     string next=longestNiceSubstring(s.substr(i+1));
+        //     return prev.size()>=next.size()?prev:next;
+        // }
+        // return s;
         if(s.size()<2) return "";
         unordered_set<char>set;
         for(int i=0;i<s.size();i++)
@@ -9,13 +26,13 @@ public:
         }
         for(int i=0;i<s.size();i++)
         {
-            if((set.find(tolower(s[i]))!=set.end()) && (set.find(toupper(s[i])))!=set.end())
+            if((set.find(tolower(s[i]))==set.end()) || (set.find(toupper(s[i])))==set.end())
             {
-                continue;
-            }
-            string prev=longestNiceSubstring(s.substr(0,i));
+                string prev=longestNiceSubstring(s.substr(0,i));
             string next=longestNiceSubstring(s.substr(i+1));
             return prev.size()>=next.size()?prev:next;
+            }
+            
         }
         return s;
     }
