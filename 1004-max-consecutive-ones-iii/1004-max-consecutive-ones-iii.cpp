bc8b437288c967fbdb976897c165847446e91a16
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-03-17 16:55:18 +0530

    Time: 108 ms (28.79%), Space: 55.3 MB (93.00%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    int longestOnes(vector<int>& A, int K) {
-        // int n=nums.size();
-        // int i=0,j=0,ans=0,zeroes=0,ones=0;
-        // while(i<n && j<n)
-        // {
-        //     if(nums[j]==0)
-        //     {
-        //         zeroes++;
-        //         j++;
-        //     }
-        //     while(zeroes>k)
-        //     {
-        //         if(nums[i]==0) zeroes--;
-        //         i++;
-        //         j++;
-        //     }
-        //     ans=max(ans,j-i+1);
-        // }
-        // return ans;
-        
-        int zeroCount = 0, l = 0;
-        int maxLen = 0;
-        for( int r = 0 ; r < A.size() ; ++r ) {
-            if( A[ r ] == 0 ) ++zeroCount;
-            while( zeroCount > K ) {
-                if( A[ l ] == 0 ) --zeroCount;
-                ++l;
+    int longestOnes(vector<int>& nums, int k) {
+        int n=nums.size();
+        int i=0,j=0,ans=0,zeroes=0,ones=0;
+        while(i<n && j<n)
+        {
+            if(nums[j]==0)
+            {
+                zeroes++;
             }
-            maxLen = max( maxLen, r - l + 1 );
+            while(zeroes>k)
+            {
+                if(nums[i]==0) zeroes--;
+                i++;
+            }
+            ans=max(ans,j-i+1);
+            j++;
         }
-        return maxLen;
+        return ans;
+        
+        // int zeroCount = 0, l = 0;
+        // int maxLen = 0;
+        // for( int r = 0 ; r < A.size() ; ++r ) {
+        //     if( A[ r ] == 0 ) ++zeroCount;
+        //     while( zeroCount > K ) {
+        //         if( A[ l ] == 0 ) --zeroCount;
+        //         ++l;
+        //     }
+        //     maxLen = max( maxLen, r - l + 1 );
+        // }
+        // return maxLen;
     }
 };
