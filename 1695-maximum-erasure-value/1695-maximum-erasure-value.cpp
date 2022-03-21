283afc4ffc7713c44cb2ca13942f88beecc3dcb5
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-03-21 23:50:57 +0530

    Time: 824 ms (10.11%), Space: 127.3 MB (14.16%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -28,18 +28,5 @@ public:
             
         }
         return mxsum;
-        // int result = 0;
-        // unordered_set<int> hset;
-        // for (int i = 0, j = 0, win = 0; j < nums.size(); j++) {
-        //     while (hset.find(nums[j]) != hset.end()) {
-        //         hset.erase(nums[i]); 
-        //         win -= nums[i];
-        //         i++;
-        //     }
-        //     hset.insert(nums[j]);
-        //     win += nums[j];
-        //     result = max(result, win);
-        // }
-        // return result;
     }
 };
