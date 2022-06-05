4f8fd78101b050706af8168468dee706ac645d87
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-06-05 19:33:29 +0530

    Added solution - LeetHub

--- Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
+++ Nth Fibonacci Number - GFG/nth-fibonacci-number.cpp
@@ -8,19 +8,18 @@ using namespace std;
 class Solution {
   public:
   
-    long long int f(long long int n,vector<long long int>&dp)
-    {
-        if(n<=1){
-            //dp[n]=n;
-            return n;
-        }
-        if(dp[n]!=-1) return dp[n];
-        return dp[n]=(f(n-1,dp)+f(n-2,dp))%1000000007;
-    }
+    
     long long int nthFibonacci(long long int n){
         // code here
         vector<long long int>dp(n+1,-1);
-        return f(n,dp)%1000000007;
+        dp[0]=0;
+        dp[1]=1;
+        
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
+        }
+        return dp[n]%1000000007;
     }
 };
 
