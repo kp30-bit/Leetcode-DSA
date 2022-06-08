cb2db54385703611e8945a4592871fd3dcae493a
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-06-09 01:41:25 +0530

    Time: 8 ms (62.48%), Space: 10.7 MB (69.34%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        sort(candidates.begin(),candidates.end());
+        vector<int>temp;
+        solve(0,candidates,ans,temp,target);
+        return ans;
+    }
+    void solve(const int ind,vector<int>&candidates,vector<vector<int>>&ans,vector<int>&temp, const int target)
+    {
+        if(target==0)
+        {
+            ans.push_back(temp);
+            return;
+        }else{
+            for(int i=ind;i<candidates.size();i++)
+            {
+                if(candidates[i]>target) return;
+                if(i && candidates[i]==candidates[i-1] &&i>ind) continue;
+               
+                temp.push_back(candidates[i]);
+                solve(i+1,candidates,ans,temp,target-candidates[i]);
+                temp.pop_back();
+            }
+        }
+    }
+};
