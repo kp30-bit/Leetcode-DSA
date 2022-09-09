6aad613487c4fd20e61c7a18393cd2544a2a4ae9
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-10 02:07:33 +0530

    Time: 72 ms (5.67%), Space: 23.3 MB (14.12%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -10,22 +10,13 @@
 
 class Solution {
 public:
-    
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* n1, TreeNode* n2) {
-        if(root==NULL) return NULL;
-        // if(root->val==p->val || root->val==q->val)
-        //     return root;
-        // TreeNode *l=lowestCommonAncestor(root->left,p,q);
-        // TreeNode *r=lowestCommonAncestor(root->right,p,q);
-        // if(l&&r) return root;
-        // if(l) return l;
-        // return r;
-        if(n1->val<root->val && n2->val<root->val){
-            return lowestCommonAncestor(root->left,n1,n2);
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root==nullptr) return nullptr;
+        if(p->val<root->val && q->val<root->val){
+            return lowestCommonAncestor(root->left,p,q);
         }
-        else if(n1->val>root->val && n2->val>root->val)
-        {
-            return lowestCommonAncestor(root->right,n1,n2);
+        else if(p->val>root->val && q->val>root->val){
+            return lowestCommonAncestor(root->right,p,q);
         }
         else return root;
     }
