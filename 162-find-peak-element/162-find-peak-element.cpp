391e44d210249cbbbe642e17e1c6b6e240c4c184
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-12 00:43:44 +0530

    Time: 3 ms (92.58%), Space: 8.7 MB (99.51%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -7,7 +7,6 @@ public:
         if(n==1) return 0;                  
 		if(nums[0]> nums[1]) return 0;     
         if(nums[n-1] > nums[n-2]) return n-1;
-        if(nums.size()==1) return 0;
         while(l<=r)
         {
             int m=l+(r-l)/2;
@@ -26,27 +25,5 @@ public:
             
         }
         return -1;
-        
-        
-//         int n = nums.size();
-//         if(n==1) return 0;                   \\ single element is always a peak element
-// 		if(nums[0]> nums[1]) return 0;     \\ check if first element satisfies the condition
-//         if(nums[n-1] > nums[n-2]) return n-1;    \\ check if last element satisfies the condition
-        
-//         int left=0,right=nums.size()-1;
-        
-//         while(left<right){
-//             int mid = left+(right-left)/2;
-//             if(nums[mid]> nums[mid-1] && nums[mid]>nums[mid+1]){    \\check if mid element satisfies the condition
-//                 return mid;
-//             }
-//             if(nums[mid] > nums[mid-1] && nums[mid]<nums[mid+1]){      
-//                 left = mid+1;     \\move left to mid+1 as nums[mid+1] > nums[mid], thus our answer  lies on right side
-//             }
-//             else{
-// 				right=mid;         \\ move right to mid as either nums[mid-1] is more than nums[mid] or nums[mid] is more than nums[mid+1] 
-//             }
-        // }
-        // return -1;
     }
 };
