5388abdb766660e817a510b42c469ee4c43614c4
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-12 00:43:06 +0530

    Time: 4 ms (78.42%), Space: 9 MB (51.47%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -8,7 +8,7 @@ public:
 		if(nums[0]> nums[1]) return 0;     
         if(nums[n-1] > nums[n-2]) return n-1;
         if(nums.size()==1) return 0;
-        while(l<r)
+        while(l<=r)
         {
             int m=l+(r-l)/2;
             if(nums[m]>nums[m-1] && nums[m]>nums[m+1])
