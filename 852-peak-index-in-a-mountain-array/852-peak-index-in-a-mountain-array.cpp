6ad6a0f2d8f4df68fd98f79b41a442906a6b9912
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-12 01:24:39 +0530

    Time: 408 ms (5.03%), Space: 59.7 MB (17.20%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -7,11 +7,21 @@ public:
         {
             int m=l+(r-l)/2;
             if(arr[m]>arr[m-1] && arr[m]>arr[m+1]) return m;
-            else if(arr[m]>arr[m-1] && arr[m]<arr[m+1]){
+            else if(arr[m]<arr[m+1]){
                 l=m+1;
             }
             else r=m;
         }
         return -1;
+        
+        
+        // while (low <= high) {
+        //     int mid = low + (high - low) / 2;
+        //     if (arr[mid] < arr[mid+1])
+        //         low = mid+1;
+        //     else
+        //         high = mid-1;
+        // }
+        // return low;
     }
 };
