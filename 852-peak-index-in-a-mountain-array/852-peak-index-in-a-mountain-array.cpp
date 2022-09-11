2e7626eaf2f37b3bd22b91e41a6bd860a71247de
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-12 01:27:04 +0530

    Time: 116 ms (97.20%), Space: 59.6 MB (93.72%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        // int l=0,n=arr.size();
-        // int r=n-1;
-        // while(l<=r)
-        // {
-        //     int m=l+(r-l)/2;
-        //     if(arr[m]>arr[m-1] && arr[m]>arr[m+1]) return m;
-        //     else if(arr[m]<arr[m+1]){
-        //         l=m+1;
-        //     }
-        //     else r=m;
-        // }
-        // return -1;
-        
-        int low = 0, high = arr.size()-1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] < arr[mid+1])
-                low = mid+1;
-            else
-                high = mid-1;
+        int l=0,n=arr.size();
+        int r=n-1;
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            // if(arr[m]>arr[m-1] && arr[m]>arr[m+1]) return m;
+            if(arr[m]<arr[m+1]){
+                l=m+1;
+            }
+            else r=m-1;
         }
-        return low;
+        return l;
+        
+        // int low = 0, high = arr.size()-1;
+        // while (low <= high) {
+        //     int mid = low + (high - low) / 2;
+        //     if (arr[mid] < arr[mid+1])
+        //         low = mid+1;
+        //     else
+        //         high = mid-1;
+        // }
+        // return low;
     }
 };
