fd623688f36527c81b285762e02f3a8a5c92ed8a
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-13 01:26:40 +0530

    Time: 245 ms (5.14%), Space: 20.9 MB (7.41%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -4,22 +4,61 @@ public:
         //greatest to right - arr1 
         //greatest to left - arr2
         //heught of water = min(arr1[i],arr2[i])-arr[i]
+        int n=height.size();
+        vector<int>gtl(n,-1);
+        vector<int>gtr(n,-1);
+        int maxx=height[0];
+        for(int i=1;i<n;i++)
+        {
+            
+            if(maxx>height[i])
+            {
+                gtl[i]=maxx;
+            }
         
-       int n = height.size();
-        vector<int> leftMax(n), rightMax(n);
-        for (int i = 1; i < n; ++i) 
-            leftMax[i] = max(height[i-1], leftMax[i-1]);
-        for (int i = n-2; i >= 0; --i) 
-            rightMax[i] = max(height[i+1], rightMax[i+1]);
-        
-        int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            int waterLevel = min(leftMax[i], rightMax[i]);
-            if (waterLevel >= height[i]) ans += waterLevel - height[i];
+            if(height[i]>maxx)
+                maxx=height[i];
         }
-        return ans;
+        int minn=height[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            if(minn>height[i])
+                gtr[i]=minn;
+            if(height[i]>minn)
+            {
+                minn=height[i];
+            }
+        }
         
-        
-        
+        vector<int>ans(n,-1);
+        for(int i=0;i<n;i++)
+        {
+            int temp=min(gtl[i],gtr[i]);
+            if(temp==-1){
+                ans[i]=-1;
+            }
+            else if(temp!=-1)
+            {
+                ans[i]=temp-height[i];
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(ans[i]!=-1)
+            {
+                sum+=ans[i];
+            }
+        }
+        for(auto i:gtl)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        for(auto i:gtr){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+        return sum;
     }
 };
