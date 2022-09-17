3851ff6110f9e4a85670e034e2a3a4c02a981a3b
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-17 16:26:31 +0530

    Time: 13 ms (40.84%), Space: 13.5 MB (21.27%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
-    
+    void solve(int ind,vector<int>candidates,vector<vector<int>>&ans,vector<int>&temp,int target)
+    {
+        if(target==0)
+        {
+            ans.push_back(temp);
+            return;
+        }
+        else{
+            for(int i=ind;i<candidates.size();i++)
+            {
+                if(candidates[i]>target) return;
+                if(i>ind && candidates[i]==candidates[i-1]) continue;
+                
+                temp.push_back(candidates[i]);
+                solve(i+1,candidates,ans,temp,target-candidates[i]);
+                temp.pop_back();
+                
+            }
+        }
+    }
     
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         vector<vector<int>>ans;
@@ -9,22 +28,5 @@ public:
         solve(0,candidates,ans,temp,target);
         return ans;
     }
-    void solve( int ind,vector<int>&candidates,vector<vector<int>>&ans,vector<int>&temp,  int target)
-    {
-        if(target==0)
-        {
-            ans.push_back(temp);
-            return;
-        }else{
-            for(int i=ind;i<candidates.size();i++)
-            {
-                if(candidates[i]>target) return;
-                if( i>ind && candidates[i]==candidates[i-1] ) continue;
-               
-                temp.push_back(candidates[i]);
-                solve(i+1,candidates,ans,temp,target-candidates[i]);
-                temp.pop_back();
-            }
-        }
-    }
+    
 };
