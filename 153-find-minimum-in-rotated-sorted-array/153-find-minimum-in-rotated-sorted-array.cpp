f0e020eea2751f6b79b84dc1e496eb85dfa9de7a
Kamal Pratik <56252860+kp30-bit@users.noreply.github.com>
2022-09-18 00:11:54 +0530

    Time: 13 ms (8.23%), Space: 10.2 MB (70.97%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.cpp
@@ -7,10 +7,15 @@ public:
         while (l < r)
         {
             mid = l + (r - l) / 2;
+
+            // got a part that is not rotated
             if (nums[l] < nums[r]) return nums[l]; 
-            else if(nums[mid] >= nums[l]) l=mid+1;
-            else r=mid;
-              
+
+            // mid is larger than right - min is in right side
+            else if (nums[mid] > nums[r]) l = mid + 1;
+
+            // mid is smaller than right - min is in left side (including mid)
+            else r = mid;
         }
         return nums[l];
     }
